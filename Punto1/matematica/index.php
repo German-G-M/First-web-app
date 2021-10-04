9610afd47563db00b07682128e376475953ad1f1
German-G-M <91800270+German-G-M@users.noreply.github.com>
2021-10-04 05:09:09 -0300

    Add files via upload

---
+++ Punto1/matematica/index.php
@@ -0,0 +1,59 @@
+<?php include "../cabecera.inc.php"; ?>
+<div class="menu">
+			<ul>
+				<li>
+				<a class="elegido" href="../"> Página Principal</a>
+				</li>
+				<br>
+				CARRERAS:
+				<br>
+				<br>
+
+				<li>
+				<a href="../informatica">Informática</a>
+				</li>
+				<li>
+				<a href="../fisica">Física</a>
+				</li>
+				<li>
+				<a href="../estadistica">Estadística</a>
+				</li>	
+				<li>
+				<a href="./">Matemática</a>
+				</li>	
+				<li>
+				<a href="../biologia">Biología</a>
+				</li>
+				<li>
+				<a href="../quimica">Química</a>
+				</li><br><br>
+				<li>
+				--------------------------------------------
+				<br><br> <br>
+				
+				<a class="elegido"> Noticias científicas</a>
+				</li><br><br>
+				<li>
+				<a class="elegido"> Preuniversitario</a>
+				</li><br><br>
+				<li>
+				<a class="elegido"> Licenciatura</a>
+				</li><br><br>
+				<li>
+				<a class="elegido"> Maestría</a>
+				</li><br><br>
+				<li>
+				<a class="elegido"> Diplomados - Cursos</a>
+				</li><br><br>
+				<li>
+				<a class="elegido"> Doctorado</a>
+				</li><br><br>
+
+			<ul>
+		</div>
+		<div class="cuerpo">
+        <img style="width:250;heigth:250; margin:20px 250px;" src="../imagenes/logomat.png"/>
+		<p style="text-align:center"> Usted está en la página de la carrera de Matemática </p><br>
+		
+		
+<?php include "../pie.inc.php"; ?>
---
+++ Punto1/pie.inc.php
@@ -0,0 +1,34 @@
+</div>
+		
+		<div class="pie">
+			<div>
+				Dirección: <br>
+				<br>
+				Av. Villazón N° 1995<br> 
+				Plaza del Bicentenario<br>
+				Zona Central, La paz - Bolivia<br>
+				<br>
+				Google maps
+			</div>
+			<div>				
+				Siguenos en : <br>
+				<br>
+				Facebook <br>
+				Youtube <br>
+				Instagram <br>
+				Twiter<br>
+			</div>
+			<div>				
+				Comunicate con nosotros: <br>
+				<br>
+				Telefono: 2612298 <br>
+				Email: dtic@umsa.bo <br>
+				<br>
+				<br>
+				<br>
+			</div>
+			<p style="text-align:center; padding:10px"> Desarrollado por: German Gutierrez Machicado  -   Todos los derechos reservados </p>
+		</div>
+		
+	</body>
+</html>
